3ca40e47fcf25c41f74828e97a682d4454afdc10
clpowe <33496540+clpowe@users.noreply.github.com>
2022-10-12 17:07:15 -0400

    connect Stripe build subscription page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,11 +8,19 @@ export default defineNuxtConfig({
   cssPath: '~/assets/css/tailwind.css',
   configPath: 'tailwind.config.js',
   exposeConfig: false,
-  config: {},
+  
   injectPosition: 0,
   viewer: true,
 },
 colorMode: {
  classSuffix: ''
 },
+runtimeConfig: {
+  private: {
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY
+  },
+  public: {
+    appDomain: process.env.APP_DOMAIN
+  }
+}
 })
